ed9462ddf698ff5dd83aea1aa923a9a7dd0de9db
leeyongjin <dragon02tv@gmail.com>
2021-08-08 23:05:27 +0900

    ⚡:: #7 FireBase json 추가

---
+++ loginandlegister/app/src/main/java/com/example/loginandlegister/WorldActivity.kt
@@ -0,0 +1,65 @@
+package com.example.loginandlegister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+
+class WorldActivity : AppCompatActivity() {
+    lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_LoginAndLegister)
+        setContentView(R.layout.activity_world)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
+            when(it.itemId){
+
+                R.id.nav_home -> replaceFragment(HomeFragment(),it.title.toString())
+                R.id.nav_credit -> replaceFragment(CreditFragment(),it.title.toString())
+                R.id.nav_sports -> replaceFragment(SportsFragment(),it.title.toString())
+                R.id.nav_romance -> replaceFragment(RomanceFragment(),it.title.toString())
+                R.id.nav_horror -> replaceFragment(HorrorFragment(),it.title.toString())
+                R.id.nav_action -> replaceFragment(ActionFragment(),it.title.toString())
+            }
+
+            true
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, title : String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
